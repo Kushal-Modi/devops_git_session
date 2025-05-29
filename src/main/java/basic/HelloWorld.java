ddd8d58eb2ee9fb0df322bbcfffa1562b15659ec
Kushal-Modi <kushalmodi2201@gmail.com>
2025-05-29 23:33:32 +0530

    x1 code changes

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -3,8 +3,7 @@ package basic;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello there!!! welcome");//from y1 branch
-
+		System.out.println("Hello there!!! welcome");//from x1 branch + from y1 branch
 	}
 	//b1 code changes
 	public void m1() {
